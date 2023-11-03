2ad46e19533cc98ff680695eedccdacec08f3f3e
Kerstin Keller <KerstinKeller@users.noreply.github.com>
2023-11-03 14:24:24 +0100

    initial revision

---
+++ custom_build/backend.py
@@ -0,0 +1,8 @@
+from setuptools import build_meta as _orig
+from setuptools.build_meta import *
+
+def get_requires_for_build_wheel(config_settings=None):
+    return _orig.get_requires_for_build_wheel(config_settings)
+    
+def get_requires_for_build_sdist(config_settings=None):
+    return _orig.get_requires_for_build_sdist(config_settings)    
